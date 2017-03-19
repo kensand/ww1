6c85c565236a0701a3fff02ccbdedfbb6a06f8b3
Kenneth Sanders <kenneth_sanders@student.uml.edu>
2017-03-19 18:39:28 -0400

    beginnings of quiz added

--- js/footer.js
+++ js/footer.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event){
     var footer = document.getElementById("footer");
     footer.appendChild(document.createElement("br"));
     footer.appendChild(document.createElement("hr"));
-    
-    footer.appendChild(document.createElement("p").appendChild(document.createTextNode("Copyright 2017 Jacob Stephens, Kenneth Sanders, Aliyu B. Zakari")));
+    var p = document.createElement("p");
+    p.appendChild(document.createTextNode("Copyright 2017 Jacob Stephens, Kenneth Sanders, Aliyu B. Zakari"));
+    footer.appendChild(p);
 });
