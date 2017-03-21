b37a174f378a66316574309c508aeb8c57f4cdee
Kenneth Sanders <kenneth_sanders@student.uml.edu>
2017-03-21 07:18:59 -0400

    added images to chapter 1 pages

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event){
+    
     var footer = document.getElementById("footer");
     footer.appendChild(document.createElement("br"));
     footer.appendChild(document.createElement("hr"));
     var p = document.createElement("p");
     p.appendChild(document.createTextNode("Copyright 2017 Jacob Stephens, Kenneth Sanders, Aliyu B. Zakari"));
     footer.appendChild(p);
+    var br = document.createElement("br");
+    footer.parentNode.insertBefore(br, footer);
+    br = document.createElement("br");
+    footer.parentNode.insertBefore(br, footer);
 });
